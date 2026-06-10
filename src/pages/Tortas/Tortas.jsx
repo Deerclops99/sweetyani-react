e41a9afb53fb7dffb612ead1b1669ec8962371be
Deerclops99 <vverareinoso@gmail.com>
2026-06-10 07:57:52 -0300

    Agregar filtro de categoria Tortas

--- src/pages/Tortas/Tortas.jsx
+++ src/pages/Tortas/Tortas.jsx
@@ -1,7 +1,40 @@
 import "./Tortas.css";
 
+import { useEffect, useState } from "react";
+import * as XLSX from "xlsx";
+import ProductCard from "../../components/ProductCard/ProductCard";
+
 function Tortas() {
-  return <h1>Tortas</h1>;
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/productos.xlsx")
+      .then((res) => res.arrayBuffer())
+      .then((ab) => {
+        const workbook = XLSX.read(ab);
+        const hoja = workbook.Sheets[workbook.SheetNames[0]];
+        const datos = XLSX.utils.sheet_to_json(hoja);
+
+        const tortas = datos.filter(
+          (producto) => producto.Categoria === "Tortas"
+        );
+
+        setProductos(tortas);
+      });
+  }, []);
+
+  return (
+    <div className="productos-container">
+      {productos.map((producto, index) => (
+        <ProductCard
+          key={index}
+          nombre={producto.Nombre}
+          precio={producto.Precio}
+          imagen={producto.Imagen}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Tortas;
